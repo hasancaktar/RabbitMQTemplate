2482ff779ce8fc8bbcd18d54c55d7ff211150b8b
Hasan Sancaktar <hasancaktar@gmail.com>
2024-09-20 17:22:12 +0300

    Add project files.

---
+++ RabbitMQ.Cunsomer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+//Bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://rpvitboc:BJQYh4M3859bK5fRTtE1oZl_qHrr36ZN@moose.rmq.cloudamqp.com/rpvitboc");
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue oluşturma
+channel.QueueDeclare(queue: "test-queue", exclusive: false); //Cunsomer'da d da kuyruk publisher'daki ile birebir aynı yapılandırmada tanımlanmalıdır.
+
+//Queue'dan mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "test-queue", false, consumer);
+
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    //Kuyruğa gelen mesajın işlendiği yer.
+    //e.body : kuyruktaki mesajın verisini bütünsel olarak getirir.
+    //e.body.span veya e.body.Array() : Kuyruktaki mesajın byte verisini getirir.
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+}
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+//Bağlantı oluşturma
+ConnectionFactory factory = new();
+
+//Bağlantıyı Aktifleştirme ve akanal açma
+factory.Uri = new("amqps://rpvitboc:BJQYh4M3859bK5fRTtE1oZl_qHrr36ZN@moose.rmq.cloudamqp.com/rpvitboc");
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//queue oluşturma
+channel.QueueDeclare(queue: "test-queue", exclusive: false);
+
+
+//queue'ye mesaj gönderme
+//RabbitMQ kuyrupa atacağı mesajları byte türünden kabul ediyor. O yüzden mesajları byte'a dönüştürmemiz gerekiyor.
+byte[] message = Encoding.UTF8.GetBytes("TEST MESAJ");
+channel.BasicPublish(exchange: "", "test-queue", body: message);
+
+
+Console.Read();
